a85e53ca09b52f2e2e534114f1824647769a373f
Tomohiro Oku <tomohiro.oku@gmail.com>
2025-09-11 14:54:12 +0000

    add header

--- trajectory_tracker/include/trajectory_tracker/path2d.h
+++ trajectory_tracker/include/trajectory_tracker/path2d.h
@@ -39,6 +39,7 @@
 
 #include <geometry_msgs/msg/pose.hpp>
 #include <nav_msgs/msg/path.hpp>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
 #include <tf2/utils.h>
 #include <trajectory_tracker_msgs/msg/path_with_velocity.hpp>
 
