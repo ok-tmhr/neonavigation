0ac2484d30f17e0e031779d289493f6e456568de
OKU Tomohiro <tomohiro.oku2023@gmail.com>
2026-03-31 22:36:30 +0900

    use neonavigation_cmake

--- neonavigation_cmake/cmake/neonavigation_add_executable.cmake
+++ neonavigation_cmake/cmake/neonavigation_add_executable.cmake
@@ -1,7 +1,7 @@
 
 macro(neonavigation_add_executable target)
 
-  cmake_parse_arguments(ARG "NO_TARGET_INCLUDE_DIRECTORIES;NO_PRIVATE" "" "LINK" ${ARGN})
+  cmake_parse_arguments(ARG "NO_TARGET_INCLUDE_DIRECTORIES;NO_PRIVATE" "" "LINK_LIBRARIES;DEPENDENCIES" ${ARGN})
 
   add_executable(${target} ${ARG_UNPARSED_ARGUMENTS})
 
@@ -10,7 +10,7 @@ macro(neonavigation_add_executable target)
   endif()
 
   set(_libs "")
-  foreach(_pkg IN LISTS ARG_LINK)
+  foreach(_pkg IN LISTS ARG_LINK_LIBRARIES)
     if(_pkg MATCHES "(_msgs|_srvs|_interfaces)$")
       list(APPEND _libs ${${_pkg}_TARGETS})
     else()
@@ -22,6 +22,14 @@ macro(neonavigation_add_executable target)
       target_link_libraries(${target} ${_libs})
     else()
       target_link_libraries(${target} PRIVATE ${_libs})
+  endif()
+
+  if(ARG_DEPENDENCIES)
+    if (ARG_NO_PRIVATE)
+      ament_target_dependencies(${target} ${ARG_DEPENDENCIES})
+    else()
+      ament_target_dependencies(${target} PUBLIC ${ARG_DEPENDENCIES})
+    endif()
   endif()
 
   list(APPEND ${PROJECT_NAME}_EXECUTABLES ${target})
--- neonavigation_cmake/cmake/neonavigation_find_build_dependencies.cmake
+++ neonavigation_cmake/cmake/neonavigation_find_build_dependencies.cmake
@@ -1,10 +1,14 @@
 
 macro(neonavigation_find_build_dependencies)
+  cmake_parse_arguments(ARG "" "" "EXCLUDE" ${ARGN})
+
   if(NOT _AMENT_PACKAGE_NAME)
     ament_package_xml()
   endif()
 
   foreach(_dep IN LISTS ${PROJECT_NAME}_BUILD_DEPENDS ${PROJECT_NAME}_BUILDTOOL_DEPENDS)
-    find_package(${_dep} REQUIRED)
+    if(NOT _dep IN_LIST ARG_EXCLUDE)
+      find_package(${_dep} REQUIRED)
+    endif()
   endforeach()
 endmacro()
