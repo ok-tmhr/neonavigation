eab2f4fb5ed3bf235e7a0d6bd2f9a77960894ab3
OKU Tomohiro <tomohiro.oku2023@gmail.com>
2026-04-01 00:11:20 +0900

    move parameter to add_library

--- neonavigation_cmake/cmake/neonavigation_add_library.cmake
+++ neonavigation_cmake/cmake/neonavigation_add_library.cmake
@@ -1,7 +1,7 @@
 
 macro(neonavigation_add_library target)
 
-  cmake_parse_arguments(ARG "NO_TARGET_INCLUDE_DIRECTORIES;NO_PRIVATE" "" "LINK_LIBRARIES;DEPENDENCIES" ${ARGN})
+  cmake_parse_arguments(ARG "NO_TARGET_INCLUDE_DIRECTORIES;NO_PRIVATE" "PARAMETER" "LINK_LIBRARIES;DEPENDENCIES" ${ARGN})
 
   add_library(${target} SHARED ${ARG_UNPARSED_ARGUMENTS})
 
@@ -10,6 +10,12 @@ macro(neonavigation_add_library target)
   endif()
 
   set(_libs "")
+
+  if(ARG_PARAMETER)
+    generate_parameter_library(${target}_parameters ${ARG_PARAMETER})
+    target_link_libraries(${target} PRIVATE ${target}_parameters)
+  endif()
+
   foreach(_pkg IN LISTS ARG_LINK_LIBRARIES)
     if(_pkg MATCHES "(.+)::(.+)")
       list(APPEND _libs ${_pkg})
@@ -21,17 +27,13 @@ macro(neonavigation_add_library target)
   endforeach()
 
   if(ARG_NO_PRIVATE)
-      target_link_libraries(${target} ${_libs})
+      target_link_libraries(${target} PUBLIC ${_libs})
     else()
       target_link_libraries(${target} PRIVATE ${_libs})
   endif()
 
   if(ARG_DEPENDENCIES)
-    if (ARG_NO_PRIVATE)
-      ament_target_dependencies(${target} ${ARG_DEPENDENCIES})
-    else()
-      ament_target_dependencies(${target} PUBLIC ${ARG_DEPENDENCIES})
-    endif()
+    ament_target_dependencies(${target} PUBLIC ${ARG_DEPENDENCIES})
   endif()
 
   list(APPEND ${PROJECT_NAME}_LIBRARIES ${target})
--- neonavigation_cmake/cmake/neonavigation_register_node.cmake
+++ neonavigation_cmake/cmake/neonavigation_register_node.cmake
@@ -1,15 +1,10 @@
 
 macro(neonavigation_register_node target)
 
-  cmake_parse_arguments(ARG "" "PARAMETER;PLUGIN" "LINK_LIBRARIES" ${ARGN})
+  cmake_parse_arguments(ARG "" "PLUGIN" "LINK_LIBRARIES" ${ARGN})
 
   set(_libs rclcpp::rclcpp rclcpp_components::component ${ARG_LINK_LIBRARIES})
   neonavigation_add_library(${target}_component ${ARG_UNPARSED_ARGUMENTS} LINK_LIBRARIES ${_libs})
-
-  if(ARG_PARAMETER)
-    generate_parameter_library(${target}_parameters ${ARG_PARAMETER})
-    target_link_libraries(${target}_component PRIVATE ${target}_parameters)
-  endif()
 
   rclcpp_components_register_node(${target}_component PLUGIN ${ARG_PLUGIN} EXECUTABLE ${target})
 endmacro()
